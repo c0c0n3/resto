14e1678b36f1a87d8e3cb4bae506151e9499139e
Andrea Falconi <andrea.falconi@gmail.com>
2022-05-14 13:34:13 +0200

    implement simple immutable set.

---
+++ util/set.go
@@ -0,0 +1,48 @@
+package util
+
+// Simple immutable set.
+type Set[T comparable] interface {
+	// Is the given value in this set?
+	Member(v T) bool
+	// Count how many elements this set holds.
+	Size() int
+	// Put the set elements in a list.
+	List() []T
+}
+
+type hashSet[T comparable] struct {
+	elements map[T]struct{}
+}
+
+// Create a map-backed Set from the given elements.
+func HashSetFromList[T comparable](vs []T) Set[T] {
+	set := &hashSet[T]{
+		elements: make(map[T]struct{}, len(vs)),
+	}
+	for _, v := range vs {
+		set.elements[v] = struct{}{}
+	}
+	return set
+}
+
+// Create a map-backed Set from the given elements.
+func HashSetFrom[T comparable](vs ...T) Set[T] {
+	return HashSetFromList(vs)
+}
+
+func (s *hashSet[T]) Member(v T) bool {
+	_, hasKey := s.elements[v]
+	return hasKey
+}
+
+func (s *hashSet[T]) Size() int {
+	return len(s.elements)
+}
+
+func (s *hashSet[T]) List() []T {
+	vs := make([]T, 0, len(s.elements))
+	for v := range s.elements {
+		vs = append(vs, v)
+	}
+	return vs
+}
---
+++ util/set_test.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestEmptySet(t *testing.T) {
+	empty := HashSetFrom[int]()
+
+	if empty.Size() != 0 {
+		t.Errorf("want: 0; got: %d", empty.Size())
+	}
+	if empty.Member(1) {
+		t.Errorf("want: no members; got: 1 is a member")
+	}
+	vs := empty.List()
+	if len(vs) != 0 {
+		t.Errorf("want: no members; got: %v", vs)
+	}
+}
+
+func TestRemoveDups(t *testing.T) {
+	set := HashSetFrom(1, 2, 2, 3, 4, 3)
+
+	if set.Size() != 4 {
+		t.Errorf("want: 4; got: %d", set.Size())
+	}
+	for k := 1; k < 5; k++ {
+		if !set.Member(k) {
+			t.Errorf("want: %d is a member; got: false", k)
+		}
+	}
+	if set.Member(5) {
+		t.Errorf("want: 5 not a member; got: 5 is a member")
+	}
+	vs := set.List()
+	if len(vs) != 4 {
+		t.Errorf("want: 4 members; got: %v", vs)
+	}
+}
