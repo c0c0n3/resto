b3f163b86c945854c879d7b940af4ecde6c97783
Andrea Falconi <andrea.falconi@gmail.com>
2022-08-20 18:15:18 +0200

    add gzip mime type.

--- mime/types.go
+++ mime/types.go
@@ -5,6 +5,7 @@ type MediaType string
 const (
 	JSON = MediaType("application/json")
 	YAML = MediaType("application/yaml")
+	GZIP = MediaType("application/gzip")
 )
 
 func (t MediaType) String() string {
